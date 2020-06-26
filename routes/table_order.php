0936cf6baa66bc0a936c0031aed7cb4c939dab3a
Daniel newns <dan@jump24.co.uk>
2020-06-26 10:48:44 +0100

    UPDATE: removing old register test class as not required and also fixing test issue

--- routes/table_order.php
+++ routes/table_order.php
@@ -2,36 +2,32 @@
 
 //This handles the magic for the stores
 Route::post('/{merchant}/table-order/add-to-order', 'Store\\Orders\\CreateController')
-    ->name('store.order.add');
+    ->name('store.table-order.order.add');
 Route::post('/{merchant}/table-order/{order}/submit-order', 'Store\\Orders\\SubmitController')
-    ->name('store.order.submit');
+    ->name('store.table-order.order.submit');
 
 Route::get('/{merchant}/table-order/{order}/order-details', 'Store\\Orders\\OrderDetails\\OrderDetailsController')
     ->middleware(['order-belongs-to-merchant'])
-    ->name('store.menu.order-details');
+    ->name('store.table-order.menu.order-details');
 
 Route::post('/{merchant}/table-order/{order}/order-details', 'Store\\Orders\\OrderDetails\\OrderDetailsPostController')
     ->middleware(['order-belongs-to-merchant'])
-    ->name('store.menu.order-details.post');
+    ->name('store.table-order.menu.order-details.post');
 
 Route::get('/{merchant}/table-order/{order}/payment', 'Store\\Orders\\Payment\\PaymentController')
-    ->name('store.menu.payment');
+    ->name('store.table-order.menu.payment');
 
 Route::post('/{merchant}/table-order/{order}/payment', 'Store\\Orders\\Payment\\PaymentPostController')
-    ->name('store.menu.payment.post');
+    ->name('store.table-order.menu.payment.post');
 
 Route::post('/{merchant}/table-order/{order}/create-intent', 'Store\\Payments\\CreateController')
-    ->name('store.payment.create');
+    ->name('store.table-order.payment.create');
 
 Route::get('/{merchant}/table-order/{order}/thanks', 'Store\\Orders\\ThankYouController')
-    ->name('store.menu.order-thank-you');
+    ->name('store.table-order.menu.order-thank-you');
 
 Route::post('/{merchant}/table-order/{order}/remove', 'Store\\Orders\\RemoveItemController')
-    ->name('store.menu.remove-item');
+    ->name('store.table-order.menu.remove-item');
 
 Route::get('/{merchant}/table-order/{order?}', 'Store\\Menu\\ViewController')
-    ->name('store.menu.view');
-
-Route::get('/{merchant}', function ($merchant) {
-    return redirect()->route('store.menu.view', [$merchant]);
-});
+    ->name('store.table-order.menu.view');
