8e730b74eb7f710f98d01b6fba82c3f8a3b4bed9
Anneka Mistry <anneka@jump24.co.uk>
2020-07-01 11:56:26 +0100

    UPDATE: footer styles

--- resources/views/global/footer.blade.php
+++ resources/views/global/footer.blade.php
@@ -1,51 +1,34 @@
 <footer class="site-footer">
-    <div class="row">
-        <div class="content">
-            <?php
-            /**
-            <div class="col col--lg-12-6 site-footer--links">
+    <div class="row flex width-full">
+        <div class="content align-items-stretch">
+            <nav class="site-footer__navigation col-span-3 m-col-span-12 sm-col-span-6">
                 <ul class="site-footer__menu">
-                    <li class="site-footer-item">
-                        <a href="" class="site-footer-link">
-                            Privacy Policy
-                        </a>
-                    </li>
-                    <li class="site-footer-item">
-                        <a href="" class="site-footer-link">
-                            Terms & Conditions
-                        </a>
-                    </li>
-                </ul>
-            </div>
-             **/?>
-            <div class="col col--lg-12-4 col--lg-offset-12-1 col--m-12-6 col--m-offset-12-4 col--sm-6-4 col--sm-offset-6-2 col--s-6-4 col--s-offset-6-2">
-                <ul class="site-footer__menu site-footer__menu--social">
-                    <li class="site-footer-item">
+                    <li class="site-footer__item">
                         <a href="https://twitter.com/go_aweder" rel="nofollow" target="_blank" class="site-footer__link">
                             Twitter
                         </a>
                     </li>
-                    <li class="site-footer-item">
+                    <li class="site-footer__item">
                         <a href="https://www.facebook.com/goaweder" rel="nofollow" target="_blank" class="site-footer__link">
                             Facebook
                         </a>
                     </li>
-                    <li class="site-footer-item">
+                    <li class="site-footer__item">
                         <a href="https://www.instagram.com/go_aweder/" rel="nofollow" target="_blank" class="site-footer__link">
                             Instagram
                         </a>
                     </li>
                 </ul>
-            </div>
-                <div class="col col--lg-12-4 col--lg-offset-12-5 col--m-12-4 col--m-offset-12-5 col--sm-6-2 col--sm-offset-6-3 col--s-6-4 col--s-offset-6-2 site-footer__bowl">
-                    <div class="bowl">
-                        <span class="icon icon--aweder">@svg('aweder-logo')</span>
-                        <span class="icon icon--cross">@svg('cross')</span>
-                        <span class="icon icon--jump">@svg('jump-logo')</span>
-                        <span class="icon icon--bowl">@svg('footer')</span>
-                    </div>
+            </nav>
+            <div class="site-footer__branding col-span-4 col-start-5 sm-col-span-2 sm-col-start-3">
+                <div class="bowl">
+                    <span class="icon icon--aweder">@svg('aweder-logo')</span>
+                    <span class="icon icon--cross">@svg('cross')</span>
+                    <span class="icon icon--jump">@svg('jump-logo')</span>
+                    <span class="icon icon--bowl">@svg('footer')</span>
                 </div>
-            <div class="col col--lg-12-3 col--lg-offset-12-10 col--m-12-10 col--m-offset-12-2 col--sm-6-6 col--sm-offset-6-1 col--s-6-6 col--s-offset-6-1 site-footer__statement">
+            </div>
+            <div class="site-footer__statement col-span-3 col-start-10 m-col-span-10 m-col-start-2 sm-col-span-6 sm-col-offset-1">
                 <p>Awe-der. is a non-profit project by <a href="https://jump24.co.uk" target="_blank">Jump24</a> to help communities and small businesses. The core code will be licensed under Open Source enabling others to contribute and enhance</p>
             </div>
         </div>
