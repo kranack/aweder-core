f08fb2b48797cb1c3234af7c5b70e8fecfb57f7c
Anneka Mistry <anneka@jump24.co.uk>
2020-07-03 16:46:10 +0100

    UPDATE: tab nav styles

--- resources/views/components/registration-steps.blade.php
+++ resources/views/components/registration-steps.blade.php
@@ -1,9 +1,9 @@
-<nav class="registration__nav col col--lg-12-8 col--lg-offset-12-3 col--m-12-10 col--m-offset-12-2 col--sm-6-6 col--sm-offset-6-1">
-    <ul>
-        <li @if ($stage === 'user-details') class="active" @endif>User Information</li>
-        <li @if ($stage === 'business-details') class="active" @endif>Business Details</li>
-        <li @if ($stage === 'contact-details') class="active" @endif>Contact Numbers</li>
-        <li @if ($stage === 'business-address') class="active" @endif>Business Address</li>
-        <?php /**<li @if ($stage === 'stripe-api') class="active" @endif>Stripe API</li>**/?>
+<nav class="tab-nav col-span-10 col-start-2 m-col-span-12 m-col-start-1 sm-col-span-6">
+    <ul class="tab-menu justify-content-between">
+        <li class="tab-menu__item @if ($stage === 'user-details')tab-menu__item--active @endif">User Information</li>
+        <li class="tab-menu__item @if ($stage === 'business-details')tab-menu__item--active @endif">Business Details</li>
+        <li class="tab-menu__item @if ($stage === 'contact-details')tab-menu__item--active @endif">Contact Numbers</li>
+        <li class="tab-menu__item @if ($stage === 'business-address')tab-menu__item--active @endif">Business Address</li>
+        <?php /**<li class="tab-menu__item @if ($stage === 'stripe-api')tab-menu__item--active @endif">Stripe API</li>**/?>
     </ul>
 </nav>
