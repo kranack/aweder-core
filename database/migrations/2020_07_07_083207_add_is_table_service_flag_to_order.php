98e00cbc51b04a4e5dff6549affce72ebd641354
James Seconde <jim@jump24.co.uk>
2020-07-07 10:51:24 +0100

    UPDATE: add failing unit tests

---
+++ database/migrations/2020_07_07_083207_add_is_table_service_flag_to_order.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsTableServiceFlagToOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('orders')) {
+            Schema::table('orders', function (Blueprint $table) {
+                if (!Schema::hasColumn('orders', 'is_table_service')) {
+                    $table->boolean('is_table_service')->after('is_delivery')->default(0);
+                }
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('orders')) {
+            Schema::table('orders', function (Blueprint $table) {
+                $table->dropColumn('is_table_service');
+            });
+        }
+    }
+}
--- tests/Unit/Middleware/IsCorrectOrderTypeMiddlewareTest.php
+++ tests/Unit/Middleware/IsCorrectOrderTypeMiddlewareTest.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Unit\Middleware;
 
+use App\Merchant;
+use App\Order;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Tests\TestCase;
 
 /**
@@ -20,34 +24,66 @@ class IsCorrectOrderTypeMiddlewareTest extends TestCase
      */
     public function canAddTableServiceItemToTableServiceOrder()
     {
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $user = factory(User::class)->create();
+        $merchant = factory(Merchant::class)->create();
+        $user->merchants()->attach($merchant->id);
+
+        $order = factory(Order::class)->create([
+            'status' => 'purchased',
+            'is_table_service' => 1,
+            'merchant_id' => $merchant->id
+        ]);
+
+        $this->assertCount(0, $order->items()->get());
+
+        $inventoryItem = $this->createAndReturnInventoryItem(['merchant_id' => $merchant->id]);
+
+        $postData = [
+            'order_no' => $order->url_slug,
+            'item' => $inventoryItem->id
+        ];
+
+        $storeRoute = route('store.menu.view', $merchant->url_slug);
+        $postRoute = route('store.table-order.order.add', $merchant->url_slug);
+        $response = $this->from($storeRoute)->post($postRoute, $postData);
+
+        $response->assertSessionHas('success', 'The item has been added to your order');
+        $response->assertStatus(SymfonyResponse::HTTP_FOUND);
+        $this->assertCount(1, $order->items()->get());
     }
 
     /**
      * @test
      */
-    public function cannotAddTableServiceItemToDeliveryOrder(): void
+    public function cannotAddDeliveryToTableServiceOrder()
     {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
+        $user = factory(User::class)->create();
+        $merchant = factory(Merchant::class)->create();
+        $user->merchants()->attach($merchant->id);
 
-    /**
-     * @test
-     */
-    public function canAddDeliveryItemToDeliveryOrder(): void
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
-    }
+        $order = factory(Order::class)->create([
+            'status' => 'purchased',
+            'is_table_service' => 1,
+            'merchant_id' => $merchant->id
+        ]);
 
-    /**
-     * @test
-     */
-    public function cannotAddDeliveryItemToTableServiceOrder(): void
-    {
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        $this->assertCount(0, $order->items()->get());
+
+        $inventoryItem = $this->createAndReturnInventoryItem(['merchant_id' => $merchant->id]);
+
+        $postData = [
+            'order_no' => $order->url_slug,
+            'item' => $inventoryItem->id
+        ];
+
+        $storeRoute = route('store.menu.view', $merchant->url_slug);
+        $postRoute = route('store.order.add', $merchant->url_slug);
+
+        $response = $this->from($storeRoute)->post($postRoute, $postData);
+
+        $response->assertSessionHas('error', 'Cannot add takeaway menu item to Table Service Order');
+        $this->assertCount(0, $order->items()->get());
+
+        $response->assertRedirect($storeRoute);
     }
 }
