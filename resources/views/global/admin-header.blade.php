bc971d5dbd32c74449548ad88b76ee035bbef612
Anneka Mistry <anneka@jump24.co.uk>
2020-07-07 16:28:31 +0100

    UPDATE: admin wrapper styles

--- resources/views/global/admin-header.blade.php
+++ resources/views/global/admin-header.blade.php
@@ -1,12 +1,11 @@
-<header class="admin-header col-span-10 col-start-3 l-col-span-9 l-col-start-4 m-col-span-12 m-col-start-1 sm-col-span-6 sm-col-start-1">
-    <div class="admin__greeting">
-        {{--@if ($merchant->logo !== null)--}}
-            {{--<span class="avatar">--}}
-                {{--<img src="{{ $merchant->getTemporaryLogoLink() }}" alt="{{ $merchant->name }}" />--}}
-            {{--</span>--}}
-        {{--@endif--}}
-        {{--<p id="admin-mobile-trigger">Welcome {{ $merchant->name }}!--}}
-            {{--<span class="icon icon--down">@svg('arrow-down')</span>--}}
-        {{--</p>--}}
+<header class="admin-header col-span-9 col-start-4 m-col-span-12 m-col-start-1 sm-col-span-6 sm-col-start-1 inline-grid grid-cols-9 m-grid-cols-12 sm-grid-cols-6">
+    <div id="admin__greeting" class="admin__greeting flex align-items-center col-span-3 col-start-7 m-col-span-5 m-col-start-8 sm-col-start-2 s-col-span-6 s-col-start-1">
+        <span class="avatar avatar--small margin-right-10">
+            <img src="{{ $merchant->getTemporaryLogoLink() }}" alt="{{ $merchant->name }}" />
+        </span>
+        <p class="admin__name flex align-items-center">Welcome {{ $merchant->name }}</p>
+        <span class="admin__trigger hidden m-flex margin-left-20">
+            <span class="admin__burger"></span>
+        </span>
     </div>
 </header>
--- resources/views/global/admin-nav.blade.php
+++ resources/views/global/admin-nav.blade.php
@@ -1,39 +1,41 @@
-<nav class="admin-nav col-span-2 row-span-2 l-col-span-3">
-    <a href="{{ route('admin.dashboard')}}" class="branding branding--admin">
-        <span class="icon icon--logo">
-            @svg('aweder-logo')
-        </span>
-    </a>
-    <ul class="admin-menu">
-        <li class="admin-menu__item">
-            <a href="{{ route('admin.dashboard') }}" class="admin-menu__link">
-                <span class="admin-menu__icon"></span>Merchant dashboard
-            </a>
-        </li>
-        <li class="admin-menu__item">
-            <a href="{{ route('admin.orders.view-all') }}" class="admin-menu__link">
-                <span class="admin-menu__icon"></span>Orders
-            </a>
-        </li>
-        <li class="admin-menu__item">
-            <a href="{{ route('admin.inventory') }}" class="admin-menu__link">
-                <span class="admin-menu__icon"></span>Inventory
-            </a>
-        </li>
-        <li class="admin-menu__item">
-            <a href="{{ route('admin.opening-hours') }}" class="admin-menu__link">
-                <span class="admin-menu__icon"></span>Opening hours
-            </a>
-        </li>
-        <li class="admin-menu__item">
-            <a href="{{ route('admin.details.edit') }}" class="admin-menu__link">
-                <span class="admin-menu__icon"></span>Business Details
-            </a>
-        </li>
-        <li class="admin-menu__item">
-            <a href="{{ route('logout') }}" class="admin-menu__link">
-                <span class="admin-menu__icon"></span> Logout
-            </a>
-        </li>
-    </ul>
+<nav id="admin-nav" class="admin-nav col-span-3 row-span-2">
+    <div class="admin-nav__wrapper m-col-span-5 m-col-start-8 sm-col-span-5 sm-col-start-2">
+        <a href="{{ route('admin.dashboard')}}" class="branding branding--admin m-hidden">
+            <span class="icon icon--logo m-hidden">
+                @svg('aweder-logo')
+            </span>
+        </a>
+        <ul class="admin-menu flex flex-col">
+            <li class="admin-menu__item">
+                <a href="{{ route('admin.dashboard') }}" class="admin-menu__link">
+                    <span class="admin-menu__icon"></span>Merchant dashboard
+                </a>
+            </li>
+            <li class="admin-menu__item">
+                <a href="{{ route('admin.orders.view-all') }}" class="admin-menu__link">
+                    <span class="admin-menu__icon"></span>Orders
+                </a>
+            </li>
+            <li class="admin-menu__item">
+                <a href="{{ route('admin.inventory') }}" class="admin-menu__link">
+                    <span class="admin-menu__icon"></span>Inventory
+                </a>
+            </li>
+            <li class="admin-menu__item">
+                <a href="{{ route('admin.opening-hours') }}" class="admin-menu__link">
+                    <span class="admin-menu__icon"></span>Opening hours
+                </a>
+            </li>
+            <li class="admin-menu__item">
+                <a href="{{ route('admin.details.edit') }}" class="admin-menu__link">
+                    <span class="admin-menu__icon"></span>Business Details
+                </a>
+            </li>
+            <li class="admin-menu__item">
+                <a href="{{ route('logout') }}" class="admin-menu__link">
+                    <span class="admin-menu__icon"></span> Logout
+                </a>
+            </li>
+        </ul>
+    </div>
 </nav>
