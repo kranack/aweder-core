dbe9b9f66a67257c9cce1e267aff8e66f7032238
Luke Armstrong <luke@jump24.co.uk>
2020-07-08 18:14:37 +0100

    UPDATE: Re-order JS so Vue is initialised first, then our JS afterwards, to avoid our click events from being stripped

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,8 +2,7 @@ import Vue from 'vue';
 import 'core-js';
 import 'regenerator-runtime/runtime';
 import UrlSlugChecker from '@/js/components/registration/UrlSlugChecker';
-import MerchantOrderTypes
-  from '@/js/components/shared/business_details/MerchantOrderTypes';
+import MerchantOrderTypes from '@/js/components/shared/business_details/MerchantOrderTypes';
 import NotificationBanner from './notification';
 import SlugChecker from './slug-checker';
 import Upload from './upload';
@@ -14,6 +13,7 @@ import OrderFilters from './order_filters';
 import AdminMenu from './admin-menu';
 import OpeningTimes from './opening-times';
 
+// SASS
 import '@/sass/app.scss';
 
 /**
@@ -21,11 +21,22 @@ import '@/sass/app.scss';
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
 window.axios = require('axios');
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// Init Vue
+Vue.config.devtools = process.env.APP_ENV === 'local';
+Vue.config.productionTip = false;
+Vue.config.silent = false;
+
+new Vue({
+  components: {
+    UrlSlugChecker,
+    'merchant-order-types': MerchantOrderTypes,
+  },
+}).$mount('#app');
+
+// Init JS
 const banner = new NotificationBanner();
 banner.init();
 
@@ -54,14 +65,3 @@ adminMenu.init();
 
 const openingTimes = new OpeningTimes();
 openingTimes.init();
-
-Vue.config.devtools = process.env.APP_ENV === 'local';
-Vue.config.productionTip = false;
-Vue.config.silent = false;
-
-new Vue({
-  components: {
-    UrlSlugChecker,
-    'merchant-order-types': MerchantOrderTypes,
-  },
-}).$mount('#app');
