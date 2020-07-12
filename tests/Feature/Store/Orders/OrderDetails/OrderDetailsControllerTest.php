3f1166b84aa6862ed4c63e1ed291b19bff614a9f
Daniel newns <dan@jump24.co.uk>
2020-07-12 19:24:08 +0100

    UPDATE: Remove test as no longer required

--- tests/Feature/Store/Orders/OrderDetails/OrderDetailsControllerTest.php
+++ tests/Feature/Store/Orders/OrderDetails/OrderDetailsControllerTest.php
@@ -28,6 +28,7 @@ class OrderDetailsControllerTest extends TestCase
     public function cannotViewOrderDetailsPageOnceOrderHasGonePastACustomerSubmissionStage()
     {
         Mail::fake();
+
         $merchant = $this->createAndReturnMerchant();
 
         $order = $this->createAndReturnOrderForStatus('Acknowledged Order', ['merchant_id' => $merchant->id]);
@@ -155,7 +156,7 @@ class OrderDetailsControllerTest extends TestCase
         $order = $this->createAndReturnOrderForStatus('Incomplete Order', ['merchant_id' => $merchant->id]);
 
         $postOrderDetailsRoute = route(
-            'store.menu.order-details',
+            'store.menu.order-details.post',
             [
                 'merchant' => $merchant->url_slug,
                 'order' => $order->url_slug
@@ -185,68 +186,10 @@ class OrderDetailsControllerTest extends TestCase
 
     /**
      * @test
-     */
-    public function settingCustomerRequestedTimeForOrderThatIsPastOrderShouldReject()
-    {
-        $this->markTestSkipped('This test isnt required as we dont validate time anymore but ive kept in just incase we bring back');
-        Mail::fake();
-
-        $merchant = $this->createAndReturnMerchant(['allow_delivery' => 1]);
-
-        $order = $this->createAndReturnOrderForStatus('Incomplete Order', ['merchant_id' => $merchant->id]);
-
-        $postOrderDetailsRoute = route(
-            'store.menu.order-details',
-            [
-                'merchant' => $merchant->url_slug,
-                'order' => $order->url_slug
-            ]
-        );
-
-        $orderDetailsRoute = route(
-            'store.menu.order-details',
-            [
-                'merchant' => $merchant->url_slug,
-                'order' => $order->url_slug
-            ]
-        );
-
-        $postDetails = [
-            'customer_name' => $this->faker->name,
-            'customer_email' => $this->faker->safeEmail,
-            'customer_address' => $this->faker->address,
-            'customer_phone' => $this->faker->phoneNumber,
-            'collection_type' => 'delivery',
-            'order_no' => $order->url_slug,
-            'order_time' => [
-                'hour' =>  Carbon::now()->subHour()->hour,
-                'minute' => Carbon::now()->subMinutes(10)->minute,
-            ]
-        ];
-
-        $response = $this->from($orderDetailsRoute)->post($postOrderDetailsRoute, $postDetails);
-
-        $response->assertRedirect($orderDetailsRoute);
-
-        $response->assertSessionHasErrors('order_time');
-
-        $this->assertDatabaseHas(
-            'orders',
-            [
-                'id' => $order->id,
-                'status' => 'incomplete'
-            ]
-        );
-    }
-
-    /**
-     * @test
      * @group OrderDelivery
      */
     public function settingIncorrectDeliveryTypeForMerchantToMakeSureAResponseIsReturned()
     {
-        $this->markTestSkipped('The flow no longer works like this, but handy for reference at the moment');
-
         Mail::fake();
 
         $merchant = $this->createAndReturnMerchant(['allow_delivery' => 0]);
@@ -254,7 +197,7 @@ class OrderDetailsControllerTest extends TestCase
         $order = $this->createAndReturnOrderForStatus('Incomplete Order', ['merchant_id' => $merchant->id]);
 
         $postOrderDetailsRoute = route(
-            'store.menu.order-details',
+            'store.menu.order-details.post',
             [
                 'merchant' => $merchant->url_slug,
                 'order' => $order->url_slug
@@ -274,7 +217,7 @@ class OrderDetailsControllerTest extends TestCase
             'customer_email' => $this->faker->safeEmail,
             'customer_address' => $this->faker->address,
             'customer_phone' => $this->faker->phoneNumber,
-            'collection_type' => 'delivery',
+            'collection_types' => 'delivery',
             'order_no' => $order->url_slug,
             'order_time' => [
                 'hour' =>  Carbon::now()->subHour()->hour,
