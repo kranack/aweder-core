4d80c7995e9d942bacf089dc60175e8b55223d1b
Anneka Mistry <anneka@jump24.co.uk>
2020-07-22 09:07:52 +0100

    UPDATE: remove test

--- tests/Unit/View/Components/DisplayItemTest.php
+++ tests/Unit/View/Components/DisplayItemTest.php
@@ -31,31 +31,4 @@ class DisplayItemTest extends TestCase
 
         $this->assertStringContainsString('cottage pie of delight', $component->render());
     }
-
-    /**
-     * @test
-     */
-    public function orderSlugDoesAppearInHiddenInput()
-    {
-        $merchant = $this->createAndReturnMerchant();
-
-        $inventory = $this->createAndReturnInventoryItem(
-            [
-                'merchant_id' => $merchant->id,
-                'description' => 'cottage pie of delight'
-            ]
-        );
-
-        $order = $this->createAndReturnOrderForStatus('Incomplete Order', ['merchant_id' => $merchant->id]);
-
-        $this->createAndReturnOrderItem(['order_id' => $order->id ]);
-
-        $component = new DisplayItem($inventory, $merchant, true, $order);
-
-        $this->assertStringContainsString('cottage pie of delight', $component->render());
-
-        $hiddenInput = '<input type="hidden" name="order_no" value="' . $order->url_slug. '" />';
-
-        $this->assertStringContainsString($hiddenInput, $component->render());
-    }
 }
