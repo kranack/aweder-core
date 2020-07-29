3f4ea9b5ad660a5172d5046d12f0f32f1b1fe42d
Anneka Mistry <anneka@jump24.co.uk>
2020-07-29 12:04:01 +0100

    UPDATE: change a to span for clickable outer block

--- resources/js/components/admin/inventory/Categories.vue
+++ resources/js/components/admin/inventory/Categories.vue
@@ -2,9 +2,9 @@
   <div class="inventory__categories inline-flex flex-col width-full">
     <header class="inventory__category-name">
       <h2 class="header-three">Starters</h2>
-      <a href="" class="button button--small button-outline--silver">
+      <span href="" class="button button--small button-outline--silver">
         <span class="button__content">Edit</span>
-      </a>
+      </span>
     </header>
     <div class="inventory__group inline-flex flex-col width-full">
       <edit-item></edit-item>
